f87b578022350b5563c8d2db51007c191d582332
hejianxian <531601727@qq.com>
2021-01-16 23:59:50 +0800

    chore: update shell script

--- addBaiduTongJi.sh
+++ addBaiduTongJi.sh
@@ -4,6 +4,7 @@ basepath=$(cd `dirname $0`; pwd)
 sourcePath=$basepath'/docs/.vuepress/dist/'
 
 echo "添加百度统计"
+echo $sourcePath
 cd $sourcePath
 sed -i '' '/<html/a\
 <script>var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?d4d09268a04f5592cb8426d540e51d00";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s);})();</script>' index.html
