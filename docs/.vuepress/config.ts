e3f5f250435fde7d9639edb47d10bf173177b020
Hejx <531601727@qq.com>
2022-01-20 12:49:25 +0800

    refactor: 调整base路径

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,7 +5,7 @@ import { sidebar } from './sidebar';
 import { navbar } from './navbar';
 
 export default defineUserConfig<DefaultThemeOptions>({
-  base: '/next/',
+  base: '/',
   lang: 'zh-CN',
   locales: {
     '/': {
