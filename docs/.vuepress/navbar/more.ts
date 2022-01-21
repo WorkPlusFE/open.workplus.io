92df6e44b8cf2358888cd9c01560063e6fc48d64
Hejx <531601727@qq.com>
2022-01-21 11:23:36 +0800

    refactor: 调整kunlun官网链接

--- docs/.vuepress/navbar/more.ts
+++ docs/.vuepress/navbar/more.ts
@@ -3,7 +3,7 @@ export const more = {
   children: [
     { text: 'WorkPlus 官网', link: 'https://workplus.io/' },
     { text: 'WorkPlus Lite（开源版）', link: 'https://lite.workplus.io/' },
-    { text: '昆仑中间件平台', link: 'http://openkunlun.io/' },
+    { text: 'KunLun 中间件平台', link: 'https://openkunlun.io/' },
     { text: 'Cordova 原始文档', link: 'https://open.workplus.io/cordova/' },
     { text: '访问 旧版本', link: 'https://open.workplus.io/v4/' },
   ]
