2699f8ddf8c326dcf40d2e0b402a8ddd8f45cebd
Hejx <531601727@qq.com>
2022-01-24 09:59:16 +0800

    chore: 更新百度统计配置

--- addBaiduTongJi.sh
+++ addBaiduTongJi.sh
@@ -6,5 +6,5 @@ sourcePath=$basepath'/docs/.vuepress/dist/'
 echo "添加百度统计"
 cd $sourcePath
 sed -i '/<html/a\
-<script>var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?d4d09268a04f5592cb8426d540e51d00";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s);})();</script>' index.html
+<script>var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?b6bd74bb8ecfd3774207ee76f60af421";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s);})();</script>' index.html
 echo "添加代码执行完毕"
