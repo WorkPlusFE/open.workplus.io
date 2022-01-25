73482c94f7d695a797f687fb1b8721868f6b93df
Hejx <531601727@qq.com>
2022-01-25 13:44:15 +0800

    chore: 条件 workplus app 常见问题文档

--- docs/.vuepress/navbar/qa.ts
+++ docs/.vuepress/navbar/qa.ts
@@ -1,6 +1,7 @@
 export const qa = {
   text: '常见问题',
   children: [
+    '/qa/app',
     '/qa/light-app',
     '/qa/js-sdk',
   ]
--- docs/.vuepress/sidebar/qa.ts
+++ docs/.vuepress/sidebar/qa.ts
@@ -2,6 +2,12 @@ import type { SidebarConfigArray } from '@vuepress/theme-default';
 
 export const qa: SidebarConfigArray = [
   {
+    text: 'WorkPlus APP',
+    children: [
+      '/qa/app',
+    ]
+  },
+  {
     text: '轻应用',
     children: [
       '/qa/light-app',
